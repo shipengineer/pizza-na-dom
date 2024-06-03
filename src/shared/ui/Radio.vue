f5c69134074742a1e5a70adf1d50ae6d75e9a1ce
mav <email@example.com>
2024-06-03 21:20:34 +0300

    radio

---
+++ src/shared/ui/Radio.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="radio-container">
+        <UiButton v-for="button,index in buttons" :key="button.value" @click="clickHandler(button.value)" class="square">
+        {{ button.title }}</UiButton>
+    </div>
+</template>
+
+<script setup lang="ts">
+const props=defineProps({
+    buttons:{
+        type:Array,
+        default:[]
+    } 
+})
+const color:globalThis.Ref<string>=ref('red')
+    console.log(color)
+    
+const emits= defineEmits(['change'])
+const clickHandler=function(newValue:string){
+    emits('change',newValue)
+}
+interface Button{
+    title:string,
+    value:number| string,
+}
+</script>
+
+<style scoped>
+
+</style>
