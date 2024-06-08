49a69ccf932ccbcce55413ed5d5ebb5c74ed5467
NicolayBelyaev <nickb2147@gmail.com>
2024-06-08 22:34:36 +0300

    suggestionInput.vue works

--- src/shared/ui/suggestionInput.vue
+++ src/shared/ui/suggestionInput.vue
@@ -4,28 +4,40 @@
   // TODO: допилить автоимпорт
 
   const query = ref("");
-  let apiResponse = ref({ suggestions: [] });
+  const suggestions = ref([]);
 
   watchDebounced(
     query,
  async (query) => {
-      apiResponse = await addressSuggestions(query)},
-{ debounce: 400, maxWait: 1000}
-  )
-
-
+      suggestions.value = (await addressSuggestions(query)).suggestions;
+    },
+{ debounce: 500})
 </script>
 
+
 <template>
-  <input class="suggestion-input" v-model="query">
-  <UiBaseButton @click="console.log(apiResponse)"> Log res</UiBaseButton>
-  <div v-for="suggestion in apiResponse.suggestions">
-    {{ suggestion.data.city }}
+  <UiBaseButton @click="console.log(suggestions)"> Log res</UiBaseButton>
+  <div class="input-container form">
+    <input class="suggestion-input form" v-model="query">
+    <div v-for="suggestion in suggestions">
+      {{ suggestion.data.region }} {{ suggestion.data.city }} {{ suggestion.data.street_with_type }}
+      {{ suggestion.data.house }} {{ suggestion.data.flat }}
+    </div>
   </div>
 </template>
 
+
 <style scoped>
+  .form {
+
+  }
+  .input-container{
+    border-radius: 10px;
+    background-color: #cecece;
+    width: 300px;
+    transition: 0.3ms ease-in-out;
+  }
   .suggestion-input {
-    border: black 1px solid;
+    height: 30px;
   }
 </style>
