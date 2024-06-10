149c0ad45f765b4202cf14bcc989f0b8fe927f66
NicolayBelyaev <nickb2147@gmail.com>
2024-06-11 00:11:38 +0300

    inputSuggestionAPI.ts start developing

---
+++ app/api/config.ts
@@ -0,0 +1,6 @@
+import type {IConfig} from "~/app/api/IConfig";
+
+export const currentConfig: IConfig = {
+    url: "http://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/address",
+    token: "48bdcdb43f43be598cec53e5896e6b2b1a0eca54"
+}
