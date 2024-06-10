f1c533a5a412d50e50d05d07f863efddf559c2e9
NicolayBelyaev <nickb2147@gmail.com>
2024-06-11 00:11:19 +0300

    inputSuggestionAPI.ts start developing

---
+++ app/api/inputSuggestionAPI.ts
@@ -0,0 +1,32 @@
+import type {IConfig} from "~/app/api/IConfig";
+import {currentConfig} from "~/app/api/config";
+
+class InputSuggestionAPI {
+    addressSuggestionUrl: string
+    token: string
+
+    constructor(config: IConfig) {
+        this.addressSuggestionUrl = config.url
+        this.token = config.token
+    }
+
+    async addressSuggestions (query: string) {
+        const suggestionResponse = await fetch(this.addressSuggestionUrl, {
+            method: "POST",
+            mode: "cors",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+                "Authorization": `Token ${this.token}`
+            },
+            body: JSON.stringify({query: query})
+        })
+        if (!suggestionResponse.ok) {
+            console.log('Похоже, у нас что-то сломалось, зайдите попозже.')
+            console.log(suggestionResponse)
+        }
+        return await suggestionResponse.json();
+    }
+}
+
+export const inputSuggestionAPI = new InputSuggestionAPI(currentConfig);
