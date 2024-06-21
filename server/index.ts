f3296031e6fc14f954a8ec3ceb46a284ebb49e07
NicolayBelyaev <nickb2147@gmail.com>
2024-06-21 19:18:29 +0300

    - server: инициализация коннекта к БД НЕ падает с ошибкой (пока не понятно, работает ли она)

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,12 @@
-// import * as pge from "pg"
-// const client = new pge.Client({
-//     user: 'postgres',
-//     password: 'pizzaadmin',
-//     host: '31.128.40.45',
-//     port: 5432,
-//     database: 'postgres'
-// })
+import { createDatabase } from "db0";
+import postgresql from "db0/connectors/postgresql";
 
-
+export const db = createDatabase(
+    postgresql({
+        user: 'postgres',
+        database: 'postgres',
+        password: 'pizzaadmin',
+        host: '31.128.40.45',
+        port: 5432
+    }),
+);
